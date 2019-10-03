142653c0266e9c20f28e77e0d257e522d9836a12
Thomas Fini Hansen <xen@xen.dk>
2019-10-03 22:17:16 +0200

    Add sort constants to SearchRequest.

--- src/Request/SearchRequest.php
+++ src/Request/SearchRequest.php
@@ -25,6 +25,18 @@ use DDB\OpenPlatform\Response\SearchResponse;
  */
 class SearchRequest extends BaseRequest
 {
+    public const SORT_NONE = 'rank_none';
+    public const SORT_RANDOM = 'random';
+    public const SORT_TITLE = 'rank_title';
+    public const SORT_GENERAL = 'rank_general';
+    public const SORT_MAIN_TITLE = 'rank_main_title';
+    public const SORT_SUBJECT = 'rank_subject';
+    public const SORT_TITLE_CREATOR = 'rank_verification';
+    public const SORT_CREATOR = 'rank_creator';
+    public const SORT_DATE_DESC = 'date_descending';
+    public const SORT_ARTICLE_DATE_DESC = 'article_date_descending';
+    public const SORT_ACQUISITION_DATE_DESC = 'acquisitionDate_descending';
+
     protected $path = '/search';
     protected $properties = [
         'fields' => 'fields',
